b2fcd8965287bf91c9ed51308b253f1ad06b2fed
Gopolang  Manyane <manyaneg@gmail.com>
2026-03-24 12:05:27 +0200

    five_dim_tables

---
+++ Five_databases.sql
@@ -0,0 +1,22 @@
+SELECT TOP (1000) [date_id]
+      ,[spaza_id]
+      ,[Supplier_id]
+      ,[shipment_id]
+      ,[order_id]
+      ,[date_id2]
+      ,[Day]
+      ,[selling_price]
+      ,[cost_price]
+      ,[quantity]
+  FROM [GMSpazashop].[dbo].[sales_table]
+
+    SELECT TOP 2 * FROM [GMSpazashop].[dbo].[sales_table]
+
+  SELECT TOP 2 * FROM [GMSpazashop].[dbo].[Device_suppliers]
+
+    SELECT TOP 2 * FROM [GMSpazashop].[dbo].[spazashop2]
+
+ SELECT TOP 2 * FROM [GMSpazashop].[dbo].[shipment_table]
+
+    SELECT TOP 2 * FROM [GMSpazashop].[dbo].[Orders_table]
+
